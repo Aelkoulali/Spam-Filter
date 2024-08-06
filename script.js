4c6d2992c028bdec34ee3b4e3d31d6a0567e1e0b
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-06 18:57:37 +0200

    Access to the id from the html document

---
+++ script.js
@@ -0,0 +1,4 @@
+// Access to the id from the html document
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");

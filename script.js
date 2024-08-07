fc781ea923ea5fe0ad2a70635b110fa35468851f
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-07 13:53:18 +0200

    Add denyList variable

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const helpRegex = /please help/i;
+const helpRegex = /please help|assist me/i;
+
+const denyList =[helpRegex];
 
 const isSpam = (msg)=> helpRegex.test(msg);
 

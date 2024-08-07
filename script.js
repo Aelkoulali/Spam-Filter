aad9e6df142454223bd4fc5db6ad6dbb01a7cd1a
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-07 10:28:29 +0200

    Add addEventListener to checkMessageButton

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
+// Add addEventListener to checkMessageButton
+checkMessageButton.addEventListener("click", ()=> {
+    if(messageInput ===""){
+        alert("Please enter a message.");
+        return;
+    }
+});
